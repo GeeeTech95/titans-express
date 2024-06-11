9abbee8f3f133d0d6e06d0d9c8e709b2a984d68f
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-06-11 12:37:21 +0100

    ''

--- cargo/urls.py
+++ cargo/urls.py
@@ -7,13 +7,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    
     path('admin-access/', admin.site.urls),
     path('',include('company.urls')),
     path('',include('Users.urls')),
     path('',include('core.urls')),
     path('logistics/',include('logistics.urls')),
     path('myadmin/',include('myadmin.urls')),
-    #path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
 
 ]
 
