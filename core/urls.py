aeb279527b17277012daccdbc98af833d36891be
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-07-19 17:31:59 +0100

    ''

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,13 @@ from django.urls import path,include
 
 from . import blog,views
 
+
+
+
+
+
+
+
 urlpatterns = [
     path('login-redirect',LoginRedirect.as_view(),name = 'login-redirect'),
 
@@ -10,6 +17,7 @@ urlpatterns = [
     #BLOG
     path("blog/",blog.BlogList.as_view(),name='blog-list'),
     path("blog/<slug:slug>/",blog.BlogDetail.as_view(),name='blog-detail'),
+
 
     path("test-template/",views.TestTemplate.as_view(),name = "test-template")
 
